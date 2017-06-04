436a82cca1f96fbbba116ddf1badbe73c59e4a02
Istiak <anik.cse@gmail.com>
2017-06-04 21:14:39 +0600

    some refactoring and the end of single linked list

--- src/linkedList/LinkedListTest.java
+++ src/linkedList/LinkedListTest.java
@@ -17,36 +17,47 @@ public class LinkedListTest {
     private static Model myModel = new Model(4,"RHTDM","India");
 
     public static void main(String args[]) {
+        System.out.println("Initialization of Data in LinkedList");
         initData();
+        System.out.println("Reading Data from LinkedList");
         readList(models);
+        System.out.println("Adding Data from LinkedList");
         addData();
+
+        System.out.println("Reading Data from LinkedList");
         readList(models);
+
+        System.out.println("Searching Data from LinkedList");
         searchData(models);
+
+        System.out.println("Removing Data from LinkedList");
         removeData();
+
+        System.out.println("Reading Data from LinkedList");
         readList(models);
 
         System.out.println("Sorting started");
         sortData(models);
         System.out.println("Sorted LinkedList");
         readList(models);
-     }
+    }
 
-     private static void initData() {
-         Model arifKhan = new Model(1,"Arif Khan","Dhaka");
-         Model heroAlam = new Model(2,"Hero Alam","Bogra");
-         Model talKhan = new Model(3,"Tal Khan","Dhaka");
+    private static void initData() {
+        Model arifKhan = new Model(1,"Arif Khan","Dhaka");
+        Model heroAlam = new Model(2,"Hero Alam","Bogra");
+        Model talKhan = new Model(3,"Tal Khan","Dhaka");
 
-         models.add(arifKhan);
-         models.add(heroAlam);
-         models.add(talKhan);
-     }
+        models.add(arifKhan);
+        models.add(heroAlam);
+        models.add(talKhan);
+    }
 
     private static void addData () {
-         models.add(myModel);
-         models.addFirst(myModel);
-         models.add(3, myModel);
-         models.addLast(myModel);
-     }
+        models.add(myModel);
+        models.addFirst(myModel);
+        models.add(3, myModel);
+        models.addLast(myModel);
+    }
 
     private static void removeData () {
         if (models.contains(myModel)) {
@@ -64,7 +75,9 @@ public class LinkedListTest {
     private static void searchData(List<Model> models){
         if(models.size() > 0) {
             System.out.print("Looking for myModel");
-            models.indexOf(myModel);
+
+            int index = models.indexOf(myModel);
+            System.out.print("MyModel Found in " + index + "number");
 //          can we perform binary search on linked lists?
 //          You can do it in theory, but it will be horribly inefficient,
 //          as you will have to keep iterating through the list to find the next midpoint.
@@ -82,19 +95,14 @@ public class LinkedListTest {
         });
     }
 
-
-
-
-      private static void readList(List<Model> models) {
-          if (models.size() > 0 ) {
-              models.forEach(model -> {
-                  System.out.println(model.id +" "+ "Model Name:"+ " "+ model.name +"Model Address:"+ " " + model.address);
-              });
-          } else {
-              System.out.println("NO data in list");
-          }
-
-
-      }
+    private static void readList(List<Model> models) {
+        if (models.size() > 0 ) {
+            models.forEach(model -> {
+                System.out.println(model.id +" "+ "Model Name:"+ " "+ model.name +"Model Address:"+ " " + model.address);
+            });
+        } else {
+            System.out.println("NO data in list");
+        }
+    }
 
 }
